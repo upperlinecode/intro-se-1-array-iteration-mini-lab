b151ef8146afc6c441764befcba0d211704d11be
jolson615 <jolson615@gmail.com>
2017-08-01 11:34:01 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# intro-se-1-array-iteration-mini-lab
+# Iteration
+
+### Introduction
+
+Iterate literally means 'to perform repeatedly'. While human beings get bored of doing something repeatedly, machines excel at it.
+
+To "iterate over an array" means to perform the same task over and over again, once for each item in the array.
+
+There are tons of ways to iterate over an array, but here's a list of some of the most useful methods you may consider looking up:
+* `.each do` iteration
+* `for ___ in ____` iteration
+* `.times do` iteration
+
+### Try it out
+
+The challenges and the datasets are all in a file called `iterate.rb`, and you'll be writing your own puts or print statements.
+
+To test your code, type `ruby iterate.rb` in the console. 
---
+++ iterate.rb
@@ -0,0 +1,26 @@
+# 1. Here's a list of all of Alex's favorite foods. For each food in the list, print out a statement that says "I really love _____" and then fill in the blank with each.
+favorite_foods = ["Sushi", "Pizza", "Salmon", "Tamales dulces", "Yuca fries", "Apple pie", "Katsu curry", "Shrimp and grits", "Waffles"]
+
+
+
+# 2. Now for each food in that list, let's be specific about how MUCH Alex likes them.
+# For example, print out "Alex's #1 favorite food is Sushi" and then "Alex's #2 favorite food is Pizza" and so on until you've made an ordered list.
+# This can be done a number of different ways, but the easiest is probably the ruby each_with_index method. Consider finding that in the documentation.
+
+
+
+
+# 3. Below is a massive array of numbers. Iterate over the array and print out only the even ones.
+many_numbers = [109, 141, 44, 51, 133, 366, 339, 248, 226, 321, 97, 195, 245, 252, 238, 1, 366, 47, 189, 91, 148, 88, 194, 106, 5, 128, 165, 337, 380, 181, 143, 95]
+
+
+
+# 4. Below is a list of 99 names. Iterate over the array and find the longest name in the whole list. Then print out "The longest name in the list is ______. It has ___ letters in it."
+many_names = ["Alexa","Burke","Kasimir","Baxter","Carissa","Vielka","Derek","Jemima","Jackson","Keegan","Graham","Melissa","Jeanette","Grant","Kirsten","Naida","Brody","Ishmael","Kane","Seth","Rae","Eagan","Camille","Alana",
+  "Vance","Melinda","Tarik","Risa","Jordan","Camilla","Karly","Baker","Adena","Calvin","Kendall","Nasim","Kellie","Dana","Rhoda","Linus","Tyler","Ahmed","Dante","Shay","Lael","Tana","Claudia","Chadwick","Tara",
+  "Fulton","Justine","Malcolm","Rowan","Christopher","Ciaran","Ivan","Hiram","Blake","Colton","Nathaniel","Moses","Cynthia","George","Ignacia","Chanda","Wyatt","Amethyst","Vladimir","Adam","Boris","Joseph","Scarlett","Kieran","Curran",
+  "Dalton","Paul","Phillip","Plato","Renee","Natalie","Barbara","Keiko","Oleg","Xerxes","Caesar","Kareem","Ahmed","Charles","Cyrus","Adria","Winifred","Pandora","Wynne","Simon","Wanda","Coby","Nolan","Marsden","Courtney"]
+
+
+
+# 5. Take that same array array and sort them into two lists: one list of long names, which have more than 6 characters, and one list of short names which have 6 or fewer characters. Then print out both lists.
